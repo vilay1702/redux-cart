1ab3e6961de16e54a653b64cfc89cd4a1be4a313
Vilay <vilay17bende@gmail.com>
2021-11-27 15:45:03 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { Header, Shop, ProductPage, Cart } from "./Components";
 import "./css/index.scss";
 
